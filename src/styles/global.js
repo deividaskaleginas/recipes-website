175b6edc3dc05062d0c8bf5e4ef76c06e6b171d8
deividaskaleginas <deividas.kaleginas@gmail.com>
2023-03-03 13:49:38 +0200

    Added global styles

---
+++ src/styles/global.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body {
+    font-family: 'Poppins', sans-serif;
+}`;
+
+export default GlobalStyles;
