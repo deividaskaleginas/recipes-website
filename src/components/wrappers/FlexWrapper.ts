0b25fb1240a4c75ae1b86c0d16ebd746612a07ed
deividaskaleginas <deividas.kaleginas@gmail.com>
2023-03-04 21:15:25 +0200

    updated: FlexWrapper

--- src/components/wrappers/FlexWrapper.ts
+++ src/components/wrappers/FlexWrapper.ts
@@ -5,6 +5,7 @@ import { Box, DefaultWrapperProps } from "./WrapperTypes";
 
 interface FlexWrapperProps extends DefaultWrapperProps {
   flexDirection?: Property.FlexDirection;
+  gap?: string;
   alignItems?: Property.AlignItems;
   justifyContent?: Property.JustifyContent;
 }
@@ -12,6 +13,7 @@ interface FlexWrapperProps extends DefaultWrapperProps {
 export const FlexWrapper = styled(Box)<FlexWrapperProps>`
   display: flex;
   flex-direction: ${({ flexDirection }) => flexDirection || "row"};
+  gap: ${({ gap }) => gap || "0rem"};
   align-items: ${({ alignItems }) => alignItems || "stretch"};
   justify-content: ${({ justifyContent }) => justifyContent || "flex-start"};
 `;
