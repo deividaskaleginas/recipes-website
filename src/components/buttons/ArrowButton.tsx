bc1ba90abc5ea95d4dea33d8b7f813070d0572d4
deividaskaleginas <deividas.kaleginas@gmail.com>
2023-03-06 16:16:46 +0200

    feture: created ArrowButton and Button components

--- src/components/buttons/ArrowButton.tsx
+++ src/components/buttons/ArrowButton.tsx
@@ -1,46 +1,55 @@
 import React from "react";
-import styled from "styled-components";
-import { arrowRight } from "../../assets/svg/index";
+import { ReactNode } from "react";
+
+import styled, { css } from "styled-components";
+
 import { theme } from "../../styles/theme";
+
+import { arrowRight } from "../../assets/svg/index";
+import { Typography } from "../typography/Typography";
 // import { NormalTextBold } from "../typography/Typography";
 
-type ArrowButtonProps = {
-  children: string;
-  padding: string;
-  width: string;
-};
+interface ArrowButtonProps extends ArrowButtonStyles {
+  children: ReactNode | ReactNode[];
+}
+
+interface ArrowButtonStyles {
+  padding?: string;
+  width?: string;
+}
 
 export const ArrowButton: React.FC<ArrowButtonProps> = ({
   children,
   padding,
   width,
+  ...props
 }) => {
   return (
-    <ArrowButtonStyled padding={padding} width={width}>
-      {/* <NormalTextBold> */}
-      {children} {arrowRight}
-      {/* </NormalTextBold> */}
+    <ArrowButtonStyled padding={padding} width={width} {...props}>
+      <Typography type="normalTextBold" color="white">
+        {children}
+      </Typography>
+      {arrowRight}
     </ArrowButtonStyled>
   );
 };
 
-interface ArrowButtonStyles {
-  width?: string;
-  padding: string;
-}
+const ArrowButtonStyled = styled.button<ArrowButtonProps>`
+  ${({ width, padding }) => css`
+    display: inline-flex;
+    justify-content: space-around;
+    height: 3.375rem;
+    min-width: ${width || "15.1875rem"};
+    padding: ${padding || "0.9375rem 3.125rem"};
+    border: none;
+    border-radius: 0.625rem;
+    background-color: ${theme.colors.primary100};
+    color: ${theme.colors.white};
+    align-items: center;
 
-const ArrowButtonStyled = styled.button<ArrowButtonStyles>`
-  min-width: ${(props) => props.width};
-  border: none;
-  border-radius: 0.625rem;
-  background-color: ${() => theme.colors.primary100};
-  padding: ${(props) => props.padding};
-
-  color: ${() => theme.colors.white};
-  align-items: center;
-
-  &:hover {
-    cursor: pointer;
-    background-color: ${() => theme.colors.primary80};
-  }
+    &:hover {
+      cursor: pointer;
+      background-color: ${theme.colors.primary80};
+    }
+  `}
 `;
---
+++ src/components/buttons/Button.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { ReactNode } from "react";
+
+import styled, { css } from "styled-components";
+
+import { theme } from "../../styles/theme";
+import { Typography } from "../typography/Typography";
+
+interface ButtonProps {
+  children: ReactNode | ReactNode[];
+}
+
+export const Button: React.FC<ButtonProps> = ({ children }) => {
+  return (
+    <ButtonStyled>
+      <Typography type="smallerTextBold" color="white">
+        {children}
+      </Typography>
+    </ButtonStyled>
+  );
+};
+
+const ButtonStyled = styled.button<ButtonProps>`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  height: 2.3125rem;
+  min-width: 10.875rem;
+  border: none;
+  border-radius: 0.625rem;
+  background-color: ${theme.colors.primary100};
+  color: ${theme.colors.white};
+  align-items: center;
+
+  &:hover {
+    cursor: pointer;
+    background-color: ${theme.colors.primary80};
+  }
+`;
