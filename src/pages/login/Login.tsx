1b4edc207f5fab02ecab1db80223c5514e894fb3
deividaskaleginas <deividas.kaleginas@gmail.com>
2023-03-27 16:33:17 +0300

    feature: added Login page response

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -2,6 +2,7 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import React, { FormEvent, useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import { theme } from "styles/theme";
 import { RouteNames } from "types/routes";
 import { firebaseApp } from "utils/firebase/firebaseConfig";
 import { ArrowButton } from "../../components/buttons/ArrowButton";
@@ -10,6 +11,8 @@ import { SignInWith } from "../../components/signInWith/SignInWith";
 import { Typography } from "../../components/typography/Typography";
 import { FlexWrapper } from "../../components/wrappers/FlexWrapper";
 import UserContext from "../../contexts/userContext/userContext";
+
+import backgroundImg from "../../assets/images/background.jpg";
 
 export const Login: React.FC = () => {
   const { findUser } = useContext(UserContext);
@@ -56,54 +59,80 @@ export const Login: React.FC = () => {
   };
 
   return (
-    <FlexWrapper
-      flexDirection="column"
-      justifyContent="space-around"
-      height="100vh"
-      padding="6.25rem 1.875rem 6.25rem 2.5rem"
-    >
-      <FlexWrapper flexDirection="column">
-        <Typography type="headerTextBold" color="black">
-          Hello,
-        </Typography>
-        <Typography type="largeTextRegular" color="label">
-          Welcome Back!
-        </Typography>
-      </FlexWrapper>
-      <FlexWrapper flexDirection="column" gap="3.875rem">
-        <LoginForm onSubmit={handleSubmit}>
-          {inputs.map((input) => (
-            <FormInputs
-              key={input.id}
-              {...input}
-              value={values[input.name]}
-              onChange={onChange}
-            />
-          ))}
-          {failedLogin && (
-            <Typography type="smallerTextRegular" color="secondary100">
-              Wrong username or password
-            </Typography>
-          )}
-          <ArrowButton width="100%">Sign In</ArrowButton>
-        </LoginForm>
-        <FlexWrapper justifyContent="center">
-          <SignInWith>Or Sign in With</SignInWith>
-        </FlexWrapper>
-      </FlexWrapper>
-      <FlexWrapper justifyContent="center" gap="0.3125rem">
-        <Typography type="smallerTextSemiBold" color="black">
-          Don't have an account?
-        </Typography>
-        <NavLink to={RouteNames.REGISTER}>
-          <Typography type="smallerTextSemiBold" color="secondary100">
-            Sign up
+    <LoginSection>
+      <LoginDataBlock>
+        <FlexWrapper flexDirection="column">
+          <Typography type="headerTextBold" color="black">
+            Hello,
           </Typography>
-        </NavLink>
-      </FlexWrapper>
-    </FlexWrapper>
+          <Typography type="largeTextRegular" color="label">
+            Welcome Back!
+          </Typography>
+        </FlexWrapper>
+        <FlexWrapper flexDirection="column" gap="3.875rem">
+          <LoginForm onSubmit={handleSubmit}>
+            {inputs.map((input) => (
+              <FormInputs
+                key={input.id}
+                {...input}
+                value={values[input.name]}
+                onChange={onChange}
+              />
+            ))}
+            {failedLogin && (
+              <Typography type="smallerTextRegular" color="secondary100">
+                Wrong username or password
+              </Typography>
+            )}
+            <ArrowButton width="100%">Sign In</ArrowButton>
+          </LoginForm>
+          <FlexWrapper justifyContent="center">
+            <SignInWith>Or Sign in With</SignInWith>
+          </FlexWrapper>
+        </FlexWrapper>
+        <FlexWrapper justifyContent="center" gap="0.3125rem">
+          <Typography type="smallerTextSemiBold" color="black">
+            Don't have an account?
+          </Typography>
+          <NavLink to={RouteNames.REGISTER}>
+            <Typography type="smallerTextSemiBold" color="secondary100">
+              Sign up
+            </Typography>
+          </NavLink>
+        </FlexWrapper>
+      </LoginDataBlock>
+    </LoginSection>
   );
 };
+
+const LoginSection = styled.section`
+  height: 100vh;
+
+  @media ${theme.device.tablet} {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 1)),
+      url(${backgroundImg});
+    background-size: cover;
+  }
+`;
+
+const LoginDataBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 35rem;
+  gap: 1.5625rem;
+  padding: 6.25rem 1.875rem 6.25rem 2.5rem;
+
+  @media ${theme.device.tablet} {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    background-color: ${theme.colors.white};
+  } ;
+`;
 
 const LoginForm = styled.form`
   display: grid;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,12 +1,15 @@
 import { colors } from "./colors";
+import { DEVICE } from "./devices";
 import { typography, TypographyThemeType } from "./typography";
 
 export const theme: ThemeType = {
   colors: colors,
   typography: typography,
+  device: DEVICE,
 };
 
 interface ThemeType {
   colors: typeof colors;
   typography: TypographyThemeType;
+  device: typeof DEVICE;
 }
